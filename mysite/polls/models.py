338e22fd6a824d6013092eaa21818a0e48fcc640
Alex Guthrie <guthriea90@gmail.com>
2018-08-12 20:21:10 -0700

    Part 7: Custom admin pages

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,6 +12,10 @@ class Question(models.Model):
         # return self.date_published >= timezone.now() - datetime.timedelta(days=1)
         return now - datetime.timedelta(days=1) <= self.date_published <= now
 
+    was_published_recently.admin_order_field = 'date_published'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
     def __str__(self):
         return self.text
 
